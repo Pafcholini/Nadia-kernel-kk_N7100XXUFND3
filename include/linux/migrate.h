94d6bbcce6fb272d20cb5d04845c79bdb5cd233b
franciscofranco <franciscofranco.1990@gmail.com>
2012-09-27 02:46:27 +0100

    3.0.39

--- include/linux/migrate.h
+++ include/linux/migrate.h
@@ -6,16 +6,29 @@
 
 typedef struct page *new_page_t(struct page *, unsigned long private, int **);
 
+/*
+ * MIGRATE_ASYNC means never block
+ * MIGRATE_SYNC_LIGHT in the current implementation means to allow blocking
+ *	on most operations but not ->writepage as the potential stall time
+ *	is too significant
+ * MIGRATE_SYNC will block when migrating pages
+ */
+enum migrate_mode {
+	MIGRATE_ASYNC,
+	MIGRATE_SYNC_LIGHT,
+	MIGRATE_SYNC,
+};
+
 #ifdef CONFIG_MIGRATION
 #define PAGE_MIGRATION 1
 
 extern void putback_lru_pages(struct list_head *l);
 extern int migrate_page(struct address_space *,
-			struct page *, struct page *);
+			struct page *, struct page *, enum migrate_mode);
 #ifndef CONFIG_DMA_CMA
 extern int migrate_pages(struct list_head *l, new_page_t x,
 			unsigned long private, bool offlining,
-			bool sync);
+			enum migrate_mode mode);
 #else
 extern int migrate_pages(struct list_head *l, new_page_t x,
 			unsigned long private, bool offlining,
@@ -23,11 +36,12 @@ extern int migrate_pages(struct list_head *l, new_page_t x,
 
 extern int migrate_replace_cma_page(struct page *oldpage,
 				       struct page **newpage);
+			enum migrate_mode mode, int tries);
 #endif
 
 extern int migrate_huge_pages(struct list_head *l, new_page_t x,
 			unsigned long private, bool offlining,
-			bool sync);
+			enum migrate_mode mode);
 
 extern int fail_migrate_page(struct address_space *,
 			struct page *, struct page *);
@@ -47,7 +61,7 @@ static inline void putback_lru_pages(struct list_head *l) {}
 #ifndef CONFIG_DMA_CMA
 static inline int migrate_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
-		bool sync) { return -ENOSYS; }
+		enum migrate_mode mode) { return -ENOSYS; }
 #else
 static inline int migrate_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
@@ -55,11 +69,12 @@ static inline int migrate_pages(struct list_head *l, new_page_t x,
 
 static inline int migrate_replace_cma_page(struct page *oldpage,
 		struct page **newpage) { return -ENOSYS; }
+		enum migrate_mode mode, int tries) { return -ENOSYS; }
 #endif
 
 static inline int migrate_huge_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
-		bool sync) { return -ENOSYS; }
+		enum migrate_mode mode) { return -ENOSYS; }
 
 static inline int migrate_prep(void) { return -ENOSYS; }
 static inline int migrate_prep_local(void) { return -ENOSYS; }
